6decfcea523208e353d830272ea9b40754ce6829
volodymyr <davidvovchik@gmail.com>
2025-02-25 20:50:59 +0300

    fix bug

--- locales/i18n.ts
+++ locales/i18n.ts
@@ -5,12 +5,22 @@ import * as RNLocalize from "react-native-localize";
 import en from "./en.json";
 import uk from "./uk.json";
 
+const deviceLanguage = RNLocalize.getLocales()?.[0]?.languageTag ?? "en";
+
+const supportedLanguages = ["en", "uk"];
+
+const languageTag = supportedLanguages.includes(deviceLanguage)
+  ? deviceLanguage
+  : "en";
+
+console.log(languageTag);
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
     uk: { translation: uk },
   },
-  lng: RNLocalize.getLocales()[0].languageTag,
+  lng: languageTag,
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
